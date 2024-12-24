4782bbc81401ae2ef15739bd81aebf93b54e4b76
sachinbrahmbhatt <barotsachin739@gmail.com>
2024-12-24 20:59:20 +0530

    `Refactored Dock widget to use Stack and updated item layout`

--- lib/main.dart
+++ lib/main.dart
@@ -15,11 +15,11 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _items = [
-    {"icon": Icons.person, "name": "Contact"},
-    {"icon": Icons.message, "name": "Message"},
-    {"icon": Icons.call, "name": "Calls"},
-    {"icon": Icons.camera, "name": "Camera"},
-    {"icon": Icons.photo, "name": "Gallary"}
+    {"image": "assets/launchpad.png", "name": "Launchpad"},
+    {"image": "assets/music.png", "name": "Music"},
+    {"image": "assets/chrome.png", "name": "Google Chrome"},
+    {"image": "assets/facetime.png", "name": "Facetime"},
+    {"image": "assets/photos.png", "name": "Photos"}
   ];
 
   dynamic dragIndex;
@@ -33,143 +33,187 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Dock(
-            items: _items,
-            builder: (e, isHovered, index, onReorder) {
-              return MouseRegion(
-                onEnter: (_) => isHovered.value = true,
-                onExit: (_) => isHovered.value = false,
-                child: ValueListenableBuilder<bool>(
-                  valueListenable: isHovered,
-                  builder: (context, hover, child) {
-                    return LongPressDraggable(
-                      data: index,
-                      feedback: Material(
-                        color: Colors.transparent,
-                        child: AnimatedContainer(
-                          duration: const Duration(milliseconds: 300),
-                          curve: Curves.easeOut,
-                          constraints: const BoxConstraints(minWidth: 60),
-                          height: 60,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(8),
-                            color: Colors.primaries[
-                                e.hashCode % Colors.primaries.length],
-                          ),
-                          child: Center(
-                              child: Icon(e["icon"] as IconData,
-                                  color: Colors.white, size: 32)),
-                        ),
-                      ),
-                      childWhenDragging: SizedBox(
-                        width: 48,
-                      ),
-                      onDragStarted: () {
-                        setState(() {
-                          isReordering = true;
-                          // _items.remove(index);
-                          dragIndex = index;
-                        });
-                      },
-                      onDraggableCanceled: (velocity, offset) {
-                        setState(() {
-                          isReordering = false;
-                        });
-                      },
-                      onDragCompleted: () {
-                        setState(() {
-                          if (dragIndex > index) {
-                            index -= 1; // Move the dragged item to the left
-                          } else {
-                            index += 1; // move to the next index
-                          }
-                          isReordering = false;
-                        });
-                      },
-                      child: isReordering
-                          ? DragTarget(
-                              onAcceptWithDetails: (details) {
-                                setState(() {
-                                  print(details.data);
-                                  currentIcon = _items[index]['icon'];
-                                  _items[index]['icon'] =
-                                      _items[details.data as int]['icon']
-                                          as IconData;
-                                  _items[details.data as int]['icon'] =
-                                      currentIcon;
-                                });
-                              },
-                              builder: (BuildContext context, dynamic accept,
-                                  dynamic reject) {
-                                return Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Container(
-                                    width: 60,
-                                    height: 60,
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(8),
-                                      color: Colors.primaries[
-                                          e.hashCode % Colors.primaries.length],
-                                    ),
-                                    child: Center(
-                                        child: Icon(
-                                      dragIcon,
-                                      color: Colors.white,
-                                      size: 32,
-                                    )),
-                                  ),
-                                );
-                              },
-                            )
-                          : Tooltip(
-                              preferBelow: false,
-                              margin: const EdgeInsets.only(bottom: 20.0),
-                              padding: const EdgeInsets.only(
-                                  bottom: 8.0, top: 8.0, right: 8.0, left: 8.0),
+        body: Stack(
+          children: [
+            Image.asset(
+              "assets/back.jpg",
+              height: double.infinity,
+              fit: BoxFit.fill,
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Dock(
+                items: _items,
+                builder: (e, isHovered, index, onReorder) {
+                  return MouseRegion(
+                    onEnter: (_) => isHovered.value = true,
+                    onExit: (_) => isHovered.value = false,
+                    child: ValueListenableBuilder<bool>(
+                      valueListenable: isHovered,
+                      builder: (context, hover, child) {
+                        return LongPressDraggable(
+                          // rootOverlay: false,
+                          data: index,
+                          feedback: Material(
+                            color: Colors.transparent,
+                            child: AnimatedContainer(
+                              duration: const Duration(milliseconds: 300),
+                              curve: Curves.easeOut,
+                              constraints: const BoxConstraints(minWidth: 60),
+                              height: 60,
                               decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10.0),
-                                  color: Colors.grey.shade300,
-                                  shape: BoxShape.rectangle),
-                              message: e['name'] as String,
-                              textStyle: TextStyle(fontSize: 14.0),
-                              child: AnimatedContainer(
-                                duration: const Duration(milliseconds: 300),
-                                curve: Curves.easeOut,
-                                constraints: BoxConstraints(
-                                  minWidth: hover ? 60 : 48,
-                                ),
-                                height: hover ? 60 : 48,
-                                margin: const EdgeInsets.all(8),
-                                transform: Matrix4.translationValues(
-                                    0, hover ? -10.0 : 0.0, 0.0),
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(8),
-                                  color: Colors.primaries[
-                                      e.hashCode % Colors.primaries.length],
-                                ),
-                                child: Center(
-                                    child: Icon(e["icon"] as IconData,
-                                        color: Colors.white,
-                                        size: hover ? 32 : 24)),
+                                image: DecorationImage(
+                                    fit: BoxFit.fill,
+                                    image: AssetImage(e["image"].toString())),
+                                borderRadius: BorderRadius.circular(8),
+                                // color: Colors.primaries[
+                                //     e.hashCode % Colors.primaries.length],
                               ),
+                              // child: Center(
+                              //     child: Icon(e["icon"] as IconData,
+                              //         color: Colors.white, size: 32)),
                             ),
-                    );
-                  },
-                ),
-              );
-            },
-            onReorder: (oldIndex, newIndex) {
-              setState(() {
-                print("simple");
-                print(newIndex);
-                print(newIndex > oldIndex ? newIndex -= 1 : newIndex += 1);
-                print(newIndex);
-                // final item = _items.removeAt(oldIndex);
-                // _items.insert(newIndex, item);
-              });
-            },
-          ),
+                          ),
+                          childWhenDragging: SizedBox(
+                            width: 0,
+                          ),
+                          // onDragUpdate: (details) {
+                          //   print(index);
+                          // },
+                          onDragStarted: () {
+                            setState(() {
+                              isReordering = true;
+                              // _items.remove(index);
+                              dragIndex = index;
+                            });
+                          },
+                          onDraggableCanceled: (velocity, offset) {
+                            setState(() {
+                              isReordering = false;
+                            });
+                          },
+                          onDragCompleted: () {
+                            setState(() {
+                              if (dragIndex > index) {
+                                index -= 1; // Move the dragged item to the left
+                              } else {
+                                index += 1; // move to the next index
+                              }
+                              isReordering = false;
+                            });
+                          },
+                          child: isReordering
+                              ? DragTarget(
+                                  onMove: (details) {
+                                    currentIcon = _items[index]['image'];
+                                    _items[index]['image'] =
+                                        _items[details.data as int]['image']
+                                            as String;
+                                    _items[details.data as int]['image'] =
+                                        currentIcon;
+                                  },
+                                  onAcceptWithDetails: (details) {
+                                    setState(() {
+                                      print(details.data);
+                                      currentIcon = _items[index]['image'];
+                                      _items[index]['image'] =
+                                          _items[details.data as int]['image']
+                                              as String;
+                                      _items[details.data as int]['image'] =
+                                          currentIcon;
+                                      // print("dragIndex => ${details.data}");
+                                      // print("currentIndex => $index");
+                                      // if (details.data as int > index) {
+                                      //   _items[index]['image'] =
+                                      //       _items[details.data as int]['image']
+                                      //           as String;
+                                      // }
+                                    });
+                                  },
+                                  builder: (BuildContext context,
+                                      dynamic accept, dynamic reject) {
+                                    return Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Container(
+                                        width: 60,
+                                        height: 60,
+                                        decoration: BoxDecoration(
+                                          image: DecorationImage(
+                                              fit: BoxFit.fill,
+                                              image: AssetImage(
+                                                  e["image"].toString())),
+                                          borderRadius:
+                                              BorderRadius.circular(8),
+                                          // color: Colors.primaries[
+                                          //     e.hashCode % Colors.primaries.length],
+                                        ),
+                                        child: Center(
+                                            child: Icon(
+                                          dragIcon,
+                                          color: Colors.white,
+                                          size: 32,
+                                        )),
+                                      ),
+                                    );
+                                  },
+                                )
+                              : Tooltip(
+                                  preferBelow: false,
+                                  margin: const EdgeInsets.only(bottom: 20.0),
+                                  padding: const EdgeInsets.only(
+                                      bottom: 8.0,
+                                      top: 8.0,
+                                      right: 8.0,
+                                      left: 8.0),
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                      color: Colors.grey.shade300,
+                                      shape: BoxShape.rectangle),
+                                  message: e['name'] as String,
+                                  textStyle: TextStyle(fontSize: 14.0),
+                                  child: AnimatedContainer(
+                                    duration: const Duration(milliseconds: 300),
+                                    curve: Curves.easeOut,
+                                    constraints: BoxConstraints(
+                                      minWidth: hover ? 60 : 48,
+                                    ),
+                                    height: hover ? 60 : 48,
+                                    margin: const EdgeInsets.all(8),
+                                    transform: Matrix4.translationValues(
+                                        0, hover ? -10.0 : 0.0, 0.0),
+                                    decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                          fit: BoxFit.fill,
+                                          image: AssetImage(
+                                              e["image"].toString())),
+                                      borderRadius: BorderRadius.circular(8),
+                                      // color: Colors.primaries[
+                                      //     e.hashCode % Colors.primaries.length],
+                                    ),
+                                    // child: Center(
+                                    //     child: Icon(e["icon"] as IconData,
+                                    //         color: Colors.white,
+                                    //         size: hover ? 32 : 24)),
+                                  ),
+                                ),
+                        );
+                      },
+                    ),
+                  );
+                },
+                onReorder: (oldIndex, newIndex) {
+                  setState(() {
+                    print("simple");
+                    print(newIndex);
+                    print(newIndex > oldIndex ? newIndex -= 1 : newIndex += 1);
+                    print(newIndex);
+                    // final item = _items.removeAt(oldIndex);
+                    // _items.insert(newIndex, item);
+                  });
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
